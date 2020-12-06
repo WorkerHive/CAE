3ece498b2d9db20879971c980820b904882aacf5
Ross Leitch <professional.balbatross@gmail.com>
2020-12-06 18:44:54 +1300

    exec perms

--- step_to_gltf.cxx
+++ step_to_gltf.cxx
@@ -20,7 +20,7 @@ int main(){
 
   printf("Starting STEP processor\n");
 
-  Handle(TDocStd_Document) xdeDoc;
+  Handle(TDocStd_Document) xdeDoc = new TDocStd_Document();
 
   STEPCAFControl_Reader aReader;
   if(!(aReader.ReadFile("GA.stp") == IFSelect_RetDone)){ 
