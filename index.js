2ed90e3dc6efad5f8773a8c83723655a1752593b
Ross Leitch <professional.balbatross@gmail.com>
2020-12-07 15:27:33 +1300

    mongo

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
 var express = require('express')
 var app = express()
+const uuid = require('uuid')
 const multer = require('multer')
 var upload = multer({ dest: '/home/ubuntu/efs/inputs/' })
+var { MongoClient } = require('mongodb');
 
 const amqplib = require('amqplib/callback_api')
 
 const q = 'stp2glb';
 
-function publisher(conn){
+function publisher(conn, db){
   conn.createChannel(on_open);
   function on_open(err, ch){
     if(err != null) console.error(err)
     ch.assertQueue(q)
 
     
-
-app.route('/process')
+    app.route('/process')
       .post(upload.single('stp'), (req, res) => {
-        ch.sendToQueue(q, Buffer.from(req.file.filename))
-        res.send({success: true})
+        let model = {
+          id: uuid.v4(),
+          fileId: req.file.filename,
+          createdAt: new Date().getTime(),
+          status: "PREPROCESS"
+        }
+
+        db.collection('models').insertOne(model, (err) => {
+          if(!err){
+            ch.sendToQueue(q, Buffer.from(req.file.filename))
+            res.send({success: true, id: model.id})
+          }else{
+            res.send({error: err})
+          }
+        })
       })
 
   }
 }
+MongoClient.connect('mongodb://localhost', (err, conn) => {
+  let db = conn.db('cae')
 
-amqplib.connect('amqp://rabbitmq:rabbitmq@localhost', (err, conn) => {
-  console.log(err, conn)
-  publisher(conn)
+  amqplib.connect('amqp://rabbitmq:rabbitmq@localhost', (err, conn) => {
+    console.log(err, conn)
+    publisher(conn, db)
+  })
 })
 
 app.listen(80)
