9d5d6afcf747e27fde5ed6f6c86cf77e7333bee0
Ross Leitch <professional.balbatross@gmail.com>
2020-12-07 13:37:33 +1300

    redo

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express')
+var app = express()
+var upload = multer({ dest: '/home/ubuntu/efs/inputs/' })
+const amqplib = require('amqplib/callback_api')
+
+const q = 'stp2glb';
+
+function publisher(conn){
+  conn.createChannel(on_open);
+  function on_open(err, ch){
+    if(err != null) console.error(err)
+    ch.assertQueue(q)
+
+    
+
+app.route('/process')
+      .post(upload.single('stp'), (req, res) => {
+        console.log(req.file)
+     ch.sendToQueue(q, Buffer.from(req.file.filename))
+  })
+
+
+  }
+}
+
+amqplib.connect('amqp://localhost', (err, conn) => {
+  publisher(conn)
+})
+
+app.listen(80)
